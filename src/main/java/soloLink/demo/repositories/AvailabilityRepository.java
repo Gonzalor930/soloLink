9cad6967b5f29f862efc8446e15e7d18bbb04d16
Gonzalo <Gonzaromano35@gmail.com>
2026-03-25 11:22:16 -0300

    Refactor AvailabilityRepository for generate Slots of time

--- src/main/java/soloLink/demo/repositories/AvailabilityRepository.java
+++ src/main/java/soloLink/demo/repositories/AvailabilityRepository.java
@@ -3,5 +3,9 @@ package soloLink.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import soloLink.demo.models.Availability;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
 public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+    List<Availability> findByTeacherIdAndDayOfWeek(Long teacherId, DayOfWeek dayOfWeek);
 }
